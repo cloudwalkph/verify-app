2ee67f7b1f91372522348ba85a093812969939c8
Nina Kristel Cheng <nkmcheng@live.com>
2017-03-14 01:44:10 +0800

    home

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 import { StackNavigator } from 'react-navigation';
+import Home from './modules/home/Home';
 
 const AppNavigator = StackNavigator({
-    Home: { screen: MyTabNavigator },
+    Home: { screen: Home },
 });
 
 export default AppNavigator;
