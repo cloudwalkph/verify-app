b73d3761521b5a28808b2929570f93b2fe1cb381
Nina Kristel Cheng <nkmcheng@live.com>
2017-03-14 11:43:36 +0800

    added events in routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { StackNavigator } from 'react-navigation';
 import Home from './modules/home/Home';
+import Events from './modules/events_selector/Events';
 
 const AppNavigator = StackNavigator({
     Home: { screen: Home },
+    EventSelector: { screen: Events },
 });
 
 export default AppNavigator;
